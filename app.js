fceb204344a05cc0580c7c6cbba162858303c4a1
Eivin Adlawan <eadlawa2@gmail.com>
2020-05-21 18:17:00 -0500

    Savepoint: added link to my Google Sheet with logic of how a tetromino shape is created using the index of the square

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
+
+  //The Tetrominoes: link below to my own Google Sheet with added logic
+  // https://docs.google.com/spreadsheets/d/1u9pkr2Pgd-521Dwr_DfxNLWZpQT1GlggB1s4z8mPTGo/edit?usp=sharing
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+  console.log(lTetromino[1])
 })
