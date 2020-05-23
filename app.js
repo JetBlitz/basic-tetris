37976f2a29faaba02fc1205f1b7517f3f6732852
Eivin Adlawan <eadlawa2@gmail.com>
2020-05-23 16:07:14 -0500

    finished iTetromino array

--- app.js
+++ app.js
@@ -35,5 +35,11 @@ document.addEventListener('DOMContentLoaded', () => {
     [0, 1, width, width+1],
     [0, 1, width, width+1]
   ]
-  console.log(oTetromino[0])
+
+  const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
 })
