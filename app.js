b802ca7b13e8519a665a05f9ea9c6539692c0e53
Eivin Adlawan <eadlawa2@gmail.com>
2020-07-05 18:05:44 -0500

    Finished adding moveLeft and moveRight

--- app.js
+++ app.js
@@ -84,6 +84,12 @@ document.addEventListener("DOMContentLoaded", () => {
   function control(e) {
     if(e.keyCode === 37) {
       moveLeft()
+    } else if (e.keyCode === 38) {
+      // rotate
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
     }
   }
   document.addEventListener('keyup', control)
@@ -141,5 +147,18 @@ document.addEventListener("DOMContentLoaded", () => {
     // draw the tetromino
     draw();
   }
+
+// move the tetromino right, unless is at the edge or there is a blockage
+function moveRight() {
+  undraw()
+  const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+  if(!isAtRightEdge) currentPosition += 1
+
+  if(current.some(index => square[currentPosition + index].classList.constains('taken'))) {
+    currentPosition -= 1
+  }
+  draw()
+}
 
 });
