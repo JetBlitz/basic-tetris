37218b5776fb1907cd2ebb675499561f1c6a5511
Eivin Adlawan <eadlawa2@gmail.com>
2020-07-25 14:42:24 -0500

    Savepoint: trying to adjust eslint since I'm getting lots of error messages.

--- app.js
+++ app.js
@@ -106,41 +106,41 @@ document.addEventListener("DOMContentLoaded", () => {
   function freeze() {
     if (
       current.some((index) =>
-        squares[currentPosition + index + width].classList.contains("taken")
-      )
-    ) {
-      current.forEach((index) =>
-        squares[currentPosition + index].classList.add("taken")
-      );
-      // start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length);
-      current = theTetrominoes[random][currentRotation];
-      currentPosition = 4;
-      draw();
-    }
+      squares[currentPosition + index + width].classList.contains("taken")
+    )
+  ) {
+    current.forEach((index) =>
+    squares[currentPosition + index].classList.add("taken")
+  );
+  // start a new tetromino falling
+  random = Math.floor(Math.random() * theTetrominoes.length);
+  current = theTetrominoes[random][currentRotation];
+  currentPosition = 4;
+  draw();
+}
+}
+
+// move the tetromino left, unless at the edge or there is a blockage
+function moveLeft() {
+  // remove the shape at its current location regardless where it is
+  undraw();
+
+  // if the most left square of the tetromino is on a square that is divisible by 10 and leaves a remainder of 0, then it returns TRUE that it is at the LEFT EDGE of the grid.
+  const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
+
+  // if it's NOT at the left edge, we can move it left
+  if (!isAtLeftEdge) currentPosition -= 1; //  this is in-line syntax for if-statements (conditionals); read: if !isAtLeftEdge is true, currentPosition = currentPosition - 1. This will move our tetromino Left when we moveLeft().
+
+  // we want our tetromino to stop IF there's another tetromino already there that has already been frozen
+
+  // if some of the squares in our tetromino shape go into a space that contains the class "taken" while traveling left, move it back one space so it appears to not have moved. We want to do this for EVERY index in our tetromino shape.
+  if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+    currentPosition += 1;
   }
 
-  // move the tetromino left, unless at the edge or there is a blockage
-  function moveLeft() {
-    //remove the shape at its current location regardless where it is
-    undraw();
-
-    //if the most left square of the tetromino is on a square that is divisible by 10 and leaves a remainder of 0, then it returns TRUE that it is at the LEFT EDGE of the grid.
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
-
-    //if it's NOT at the left edge, we can move it left
-    if (!isAtLeftEdge) currentPosition -= 1; //  this is in-line syntax for if-statements (conditionals); read: if !isAtLeftEdge is true, currentPosition = currentPosition - 1. This will move our tetromino Left when we moveLeft().
-
-    //we want our tetromino to stop IF there's another tetromino already there that has already been frozen
-
-    //if some of the squares in our tetromino shape go into a space that contains the class "taken" while traveling left, move it back one space so it appears to not have moved. We want to do this for EVERY index in our tetromino shape.
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition += 1;
-    }
-
-    // draw the tetromino
-    draw();
-  }
+  // draw the tetromino
+  draw();
+}
 
 // move the tetromino right, unless is at the edge or there is a blockage
 function moveRight() {
@@ -165,5 +165,16 @@ function moveRight() {
     current = theTetrominoes[random][currentRotation]
     draw()
   }
+
+  // show up-next tetromino in mini-grid
+  const displaySquare = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
+
+  // the Tetrominos without rotations
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], // zTetromino
+  ]
 
 });
